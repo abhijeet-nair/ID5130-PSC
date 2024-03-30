8768c5cd8d9b7e5419ec0b8d25c51574885116fa
abhijeet-nair <jithu4880@gmail.com>
2024-03-30 11:15:12 +0530

    Updating T9

--- Tutorials/T9/Q2.cpp
+++ Tutorials/T9/Q2.cpp
@@ -3,7 +3,7 @@
 #include<mpi.h>
 
 int main (int argc, char* argv[]) {
-    int myid, np, i, j, m{}, n {}, lm {}, lmi {};
+    int myid, np, i, j, k, m{}, n {}, lm {}, lmi {}, ln {}, lni {};
     int dat[2] {};
 
     MPI_Init(NULL, NULL);
@@ -19,6 +19,7 @@ int main (int argc, char* argv[]) {
 
         dat[0] = m;
         dat[1] = n;
+        printf("\n");
     }
     
     MPI_Bcast(&dat, 2, MPI_INT, 0, MPI_COMM_WORLD);
@@ -26,25 +27,82 @@ int main (int argc, char* argv[]) {
     m = dat[0];
     n = dat[1];
     lmi = int(m/np);
+    lni = int(n/np);
 
-    if (myid == 0) {lm = m - lmi*(np - 1);}
-    else {lm = lmi;};
+    if (myid == 0) {
+        lm = m - lmi*(np - 1);
+        ln = n - lni*(np - 1);
+    }
+    else {
+        lm = lmi;
+        ln = lni;
+    }
 
     int lA[lm][n] {};
-    int lv[lm] {};
+    int lv[ln] {};
+    int lr[lm] {};
 
     // Considering each processor fills the data also...
     // Can be modified for the other case as well...
     for (i = 0; i < lm; i++) {
-        lv[i] = myid*10;
         for (j = 0; j < n; j++){
             lA[i][j] = myid*(j+1)*10;
         }
     }
+    
+    for (i = 0; i < np; i++) {
+        if (myid == i) {
+            for (j = 0; j < lm; j++) {
+                for (k = 0; k < n; k++) {
+                    printf("%2.0f\t",double(lA[j][k]));
+                }
+                printf("\n");
+            }
+        }
+    }
 
-    int v[m] {}, cnts[np] {}, displs[np] {};
+    for (i = 0; i < ln; i++) {
+        lv[i] = myid*10;
+    }
+
+    int v[n] {}, cnts[np] {}, displs[np] {}, r[m] {};
 
     // printf("lm = %d\n",lm);
+    for (i = 0; i < np; i++) {
+        if (i == 0) {
+            cnts[i] = n - lni*(np - 1);
+        }
+        else {
+            cnts[i] = lni;
+            displs[i] = cnts[0] + (i - 1)*lni;
+        }
+        // if (myid == 0) {
+        //     printf("cnts[%d] = %d\tdispls[%d] = %d\n",i,cnts[i],i,displs[i]);
+        // }
+    }
+    MPI_Allgatherv(&lv, ln, MPI_INT, &v, cnts, displs, MPI_INT, MPI_COMM_WORLD);
+
+    if (myid == 0) {
+        printf("\n");
+        for (i = 0; i < n; i++) {
+            printf("v[%2.0f] = %d\n",double(i),v[i]);
+        }
+    }
+
+    for (i = 0; i < lm; i++) {
+        for (j = 0; j < n; j++) {
+            lr[i] += lA[i][j]*v[j];
+        }
+    }
+
+    // for (i = 0; i < np; i++) {
+    //     if (myid == i) {
+    //         for (j = 0; j < lm; j++) {
+    //             printf("r[%2.0f] = %d\n",double(i),lr[j]);
+    //         }
+    //     }
+    // }
+
     for (i = 0; i < np; i++) {
         if (i == 0) {
             cnts[i] = m - lmi*(np - 1);
@@ -53,17 +111,17 @@ int main (int argc, char* argv[]) {
             cnts[i] = lmi;
             displs[i] = cnts[0] + (i - 1)*lmi;
         }
-        // if (myid == 0) {
-        //     printf("cnts[%d] = %d\tdispls[%d] = %d\n",i,cnts[i],i,displs[i]);
-        // }
     }
-    MPI_Allgatherv(&lv, lm, MPI_INT, &v, cnts, displs, MPI_INT, MPI_COMM_WORLD);
+
+    MPI_Allgatherv(&lr, lm, MPI_INT, &r, cnts, displs, MPI_INT, MPI_COMM_WORLD);
 
     if (myid == 0) {
+        printf("\n");
         for (i = 0; i < m; i++) {
-            printf("v[%2.0f] = %d\n",double(i),v[i]);
+            printf("r[%2.0f] = %d\n",double(i),r[i]);
         }
     }
+
     MPI_Finalize();
     return 0;
 }
