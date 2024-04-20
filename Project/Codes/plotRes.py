758b0fe40942da6ca4bc6af3a80ebff1aa71d2c1
abhijeet-nair <jithu4880@gmail.com>
2024-04-20 10:37:54 +0530

    Completed project serial

--- Project/Codes/plotRes.py
+++ Project/Codes/plotRes.py
@@ -18,18 +18,23 @@ D = np.loadtxt(file1, max_rows=Nt, skiprows=(Nt+10), usecols=1 ,delimiter=',')
 xwM = np.zeros((Nt,Nt))
 ywM = np.zeros((Nt,Nt))
 
-for i in range(Nt):
+fcnt = Nt
+for i in range(fcnt):
     sprw = (2 + i)*Nt + 11 + i # (2*Nt + 11) + i*(Nt+1)
     xwM[i][:] = np.loadtxt(file1, max_rows=Nt, skiprows=sprw, usecols=0 ,delimiter=',')
     ywM[i][:] = np.loadtxt(file1, max_rows=Nt, skiprows=sprw, usecols=1 ,delimiter=',')
 # print(xwM[Nt-1][-10:],ywM[Nt-1][-10:])
 
+# fig1, ax1 = plt.subplots()
+# pdats = ax1.scatter([],[], s=3)
 fig1 = plt.figure(figsize=(8,6))
-pdats = plt.scatter([])
-pdat = pdats[0]
+pdats = plt.scatter([], [], s=4, c='r')
+plt.xlim(-110, 10*c)
+plt.ylim(-10, 10)
+# pdat = pdats[0]
 
 def animFunc(frame):
-    pdat.set_data((xwM[frame][:], ywM[frame][:]))
+    pdats.set_offsets(np.c_[xwM[frame][:], ywM[frame][:]])
 
-anim = FuncAnimation(fig1, animFunc, frames=Nt, interval=100)
+anim = FuncAnimation(fig1, animFunc, frames=fcnt, interval=25)
 plt.show()
