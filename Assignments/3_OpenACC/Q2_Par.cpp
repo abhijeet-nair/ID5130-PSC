71ded0b0593a35e46bc1348d3ebcc6dbd751650a
abhijeet-nair <jithu4880@gmail.com>
2024-05-01 16:29:20 +0530

    Updating A3

--- Assignments/3_OpenACC/Q2_Par.cpp
+++ Assignments/3_OpenACC/Q2_Par.cpp
@@ -3,18 +3,18 @@
 #include <time.h>
 
 #define TYPE float
-#define N 10
+#define N 500
 #define sval 0.001
 #define tol 1e-6
 
-void initmult (TYPE mat[][N]) {
+void initmult (TYPE mat[N][N]) {
     #pragma acc parallel loop present(mat[:][:])
         for (int i = 0; i < N; i++) {
             #pragma acc loop vector
                 for (int j = 0; j < i; j++) {
                     // mat[i][j] = (i + j) / pow(N,2);
-                    mat[i][j] = (i + j) / (N * N);
-                    mat[j][i] = mat[i][j];
+                    mat[i][j] = TYPE(i + j) / (N * N);
+                    mat[j][i] = mat[i][j];                    
                 }
 
             mat[i][i] = 1;
@@ -30,35 +30,46 @@ TYPE myabs(TYPE a) {
     }
 }
 
-void printMat (TYPE a[][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%.4f ", a[i][j]);
+void printMat (TYPE a[N][N]) {
+    if (N > 10) {
+        printf("Avoiding printing of large matrix...\n");
+    }
+    else {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (a[i][j] < tol) {
+                    printf("0      ");
+                }
+                else {
+                    printf("%.4f ", a[i][j]);
+                }
+            }
+            printf("\n");
         }
-        printf("\n");
     }
 }
 
-void cholesky (TYPE a[N][N], TYPE l[N][N], int stat[2]) {
-    // Ensure that stat has both terms as 0
+void cholesky (TYPE a[N][N], TYPE l[N][N], int stat) {
+    // Ensure that stat is 0
     
-    #pragma acc parallel loop gang default(present)
+    #pragma acc parallel loop gang default(present) copyout(stat)
         for (int i = 0; i < N; i++) {        
             #pragma acc loop vector
                 for (int j = 0; j < i; j++) {
                     if (myabs(a[i][j] - a[j][i]) > tol) {
-                        stat[0] = 1;
+                        stat = 1;
                     }
                 }
         }
     
-    if (stat[0] == 1) {
+    if (stat == 1) {
         printf("Matrix is not symmetric...\n");
         return;
     }
+    stat = 0;
     double sum = 0;
 
-    #pragma acc data present(a[:][:], l[:][:], stat[:]) copyin(stat)
+    #pragma acc data present(a[:][:], l[:][:]) copyin(sum) copy(stat)
     {
         #pragma acc parallel num_gangs(1)
         {
@@ -84,7 +95,7 @@ void cholesky (TYPE a[N][N], TYPE l[N][N], int stat[2]) {
                     }
 
                 if (a[i][i] - sum < 0) {
-                    stat[1] = 1;
+                    stat = 1;
                 }
 
                 l[i][i] = sqrt(a[i][i] - sum);
@@ -97,17 +108,31 @@ void cholesky (TYPE a[N][N], TYPE l[N][N], int stat[2]) {
 int main () {
     TYPE a[N][N];
     TYPE l[N][N] {};
-    int stat[2] {};
+    int stat {};
 
-    #pragma acc data copyout(a[:][:], l[:][:], stat[:])
+    #pragma acc data copyout(a[:][:], l[:][:]) copy(stat)
     {
         initmult(a);
         cholesky(a, l, stat);
     }
+    
+    if (stat == 1) {
+        printf("Matrix is not positive definite...\n");
+    }
+    else {
+        printMat(a);
+        printf("\n");
+        printMat(l);
+        printf("\n");
+    }
 
-    printMat(a);
-    printf("\n");
-    printMat(l);
-
+    int r1, r2;
+    for (int i = 0; i < 3; i++) {
+        r1 = rand()%(N-1);
+        r2 = rand()%(N-1);
+        printf("(%d, %d) ==>\t", r1, r2);
+        printf("a = %.4f\tl = %.4f\n",a[r1][r2],l[r1][r2]);
+    }
+    
     return 0;
 }
